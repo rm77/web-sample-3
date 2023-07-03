30964adb88b17d26c20447d3a77da3b93520b76e
Roy <royyana@gmail.com>
2023-07-03 18:17:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <html>
 <body>
 Selamat Datang di Kelas Cloud Computing 2023<br>
-<a href="info.php">Info</a><br>
+<p>
+  Dalam MK ini akan dibahas beberapa hal 
+  <ul>
+    <li>Pembuatan VM secara otomatis</li>
+    <li>Otomasi menggunakan cloud native dan container</li>
+  </ul>
+</p>
+  <a href="info.php">Info</a><br>
 </body>
 </html>
